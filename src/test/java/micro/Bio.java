aafdb0bd3bb5cb021e744c2339c24bb2c9a27ba2
PushmithaV <pushmitha.venugopal@gmail.com>
2019-08-16 15:29:34 +0530

    Maven

---
+++ src/test/java/micro/Bio.java
@@ -0,0 +1,21 @@
+package micro;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+public class Bio {
+	static
+	{
+		System.setProperty("webdriver.chrome.driver", "./softwares/chromedriver.exe");
+	}
+
+	@Test
+	public void execute()
+	{
+		WebDriver driver= new ChromeDriver();
+		driver.get("https://www.google.co.in");
+		System.out.println(driver.getTitle());
+		driver.close();
+	}
+}
